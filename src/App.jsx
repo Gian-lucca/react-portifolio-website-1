652202002f2f69bc41eaa8645d29478cc28baf9d
Gianlucca Augusto <89024507+Gian-lucca@users.noreply.github.com>
2023-01-22 04:06:39 -0300

    Commit inicial 22/01/23

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+
+    </>
+  )
+}
+
+export default App
