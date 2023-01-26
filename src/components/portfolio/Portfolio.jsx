08be800b9d9cf1749301c449915edb6e47422622
Gianlucca Augusto <89024507+Gian-lucca@users.noreply.github.com>
2023-01-25 23:12:21 -0300

    Portfolio / serviços / depoimentos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5> Trabalhos Recentes </h5>
+      <h2>Portfólio</h2>
+
+      <div className="container portfolio__container">
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Título de item de Portfólio</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target={'_blank'}>Github</a>
+          <a href="https://gianlucca-augusto.vercel.app/" className='btn btn-primary' target={'_blank'}>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Título de item de Portfólio</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target={'_blank'}>Github</a>
+          <a href="https://gianlucca-augusto.vercel.app/" className='btn btn-primary' target={'_blank'}>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>Título de item de Portfólio</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target={'_blank'}>Github</a>
+          <a href="https://gianlucca-augusto.vercel.app/" className='btn btn-primary' target={'_blank'}>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>Título de item de Portfólio</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target={'_blank'}>Github</a>
+          <a href="https://gianlucca-augusto.vercel.app/" className='btn btn-primary' target={'_blank'}>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>Título de item de Portfólio</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target={'_blank'}>Github</a>
+          <a href="https://gianlucca-augusto.vercel.app/" className='btn btn-primary' target={'_blank'}>Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>Título de item de Portfólio</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target={'_blank'}>Github</a>
+          <a href="https://gianlucca-augusto.vercel.app/" className='btn btn-primary' target={'_blank'}>Demo</a>
+          </div>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+
+import {  Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name:'Nicole Pereira',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit, Quasi quis cumque adipisci at ex obcaecati odit fugit, veritatis unde illum perferendis atque veniam. Nobis a laudantium fuga quasi architecto non!'
+  },
+  {
+    avatar: AVTR2,
+    name:'Luiz Felipe',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit, Quasi quis cumque adipisci at ex obcaecati odit fugit, veritatis unde illum perferendis atque veniam. Nobis a laudantium fuga quasi architecto non!'
+  },
+  {
+    avatar: AVTR3,
+    name:'Giancarlo Oliveira',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit, Quasi quis cumque adipisci at ex obcaecati odit fugit, veritatis unde illum perferendis atque veniam. Nobis a laudantium fuga quasi architecto non!'
+  },
+  {
+    avatar: AVTR4,
+    name:'Isabela Pereira',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit, Quasi quis cumque adipisci at ex obcaecati odit fugit, veritatis unde illum perferendis atque veniam. Nobis a laudantium fuga quasi architecto non!'
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Depoimentos</section>
+    <section id='testimonials'>
+      <h5>Review dos Cliebtes</h5>
+      <h2>Depoimentos</h2>
+
+      <Swiper className="container testimonials__container"
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) =>{
+            return(
+              <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar}/>
+              </div>
+              <h5 className='client__name'> {name}</h5>
+                <small className='client__review'>{review}</small>
+            </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
